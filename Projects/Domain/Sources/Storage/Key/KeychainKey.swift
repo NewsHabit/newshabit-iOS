abecaf7e9cd802f162388b5804e37174ff30323d
noeyiz <lamoureclatant@gmail.com>
2025-10-26 17:12:03 +0900

    feat: SecureStorage 및 KeychainKey 정의

---
+++ Projects/Domain/Sources/Storage/Key/KeychainKey.swift
@@ -0,0 +1,16 @@
+//
+//  KeychainKey.swift
+//  Domain
+//
+//  Created by 지연 on 10/26/25.
+//
+
+import Foundation
+
+public enum KeychainKey: String {
+    case fcmToken
+    case deviceId
+    case socialId
+    case accessToken
+    case refreshToken
+}
